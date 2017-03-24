576758efebb1e9da8e889348896fb0e9dc285366
Azzahrah <joko.pitoyo@gmail.com>
2017-03-25 06:52:25 +0700

    Add Objects Definition

---
+++ UMyClass.pas
@@ -0,0 +1,64 @@
+unit UMyClass;
+
+interface
+
+uses System.Classes,System.Generics.Collections;
+
+implementation
+
+type
+  TUser = class(TObject)
+    constructor Create(AOwner: TComponent); override;
+  private
+  public
+    id: Integer;
+    login: String;
+    real_name: String;
+    level: String;
+    level_id:word;
+    active: byte;
+  end;
+
+  TCallbackLoadUser = procedure(logs: TStringList;users:TList<TUser>);
+
+  TMyThread = class(TTHread)
+    constructor Create(_Suspended: Boolean);
+  private
+    Procedure LoadUser;
+    Procedure LoadVehicle;
+  protected
+    procedure Execute; override;
+  end;
+
+  { TMyThread Definition }
+
+  { TMyThread }
+
+constructor TMyThread.Create(_Suspended: Boolean);
+begin
+  inherited Create(_Suspended);
+end;
+
+Procedure TMyThread.LoadUser;
+begin
+  //
+end;
+
+Procedure TMyThread.LoadVehicle;
+begin
+  //
+end;
+
+Procedure TMyThread.Execute;
+begin
+  //
+end;
+
+{ TUser }
+
+constructor TUser.Create(AOwner: TComponent);
+begin
+  inherited;
+end;
+
+end.
